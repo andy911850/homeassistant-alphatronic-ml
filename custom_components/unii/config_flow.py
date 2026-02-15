6c754ddcf6cc8b7127c83e72f9f5214f053fe722
Unii Integration Author <git@example.com>
2026-02-15 03:43:57 +0100

    Fix 500 Error by correcting OptionsFlow init (v1.4.2)

--- custom_components/unii/config_flow.py
+++ custom_components/unii/config_flow.py
@@ -63,7 +63,7 @@ class UniiOptionsFlowHandler(config_entries.OptionsFlow):
 
     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
         """Initialize headers."""
-        super().__init__(config_entry)
+        self.config_entry = config_entry
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
